d3934416fb7f664ce228b5faee8cd3708905759a
SparrowMike <michal.m_wroblewski@hotmail.com>
2023-09-06 00:20:42 +0800

    feat: 🎸 added chord tone feature plus started on documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+/**
+ * Guitar Chord Visualization App
+ * This file contains the main App component and related functionalities.
+ */
+
 import { useState, useEffect } from 'react'
 import { isMobile } from 'react-device-detect';
-import { get as getChordData, chordScales } from '@tonaljs/chord';
+import { get as getChordData } from '@tonaljs/chord'; //? ----- tbc { chordScales }
 import { detect as detectChord } from "@tonaljs/chord-detect";
-import { majorKey, minorKey } from '@tonaljs/key';
+// import { majorKey, minorKey } from '@tonaljs/key'; //? ----- tbc
 
+/**
+ * An array representing the chromatic scale with sharp notation.
+ */
 const chromaticSharp = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
+/**
+ * A mapping of enharmonic equivalents.
+ */
 const enharmonicMap: { [key: string]: string } = {
   "E#": "F",
   "B#": "C",
@@ -16,6 +27,9 @@ const enharmonicMap: { [key: string]: string } = {
   "Gb": "F#",
 }
 
+/**
+ * Initial state of guitar notes with active status.
+ */
 const initialNotes = {
   e: { E: { active: false }, F: { active: false }, 'F#': { active: false }, G: { active: false }, 'G#': { active: false }, A: { active: false }, 'A#': { active: false }, B: { active: false }, C: { active: false }, 'C#': { active: false }, D: { active: false }, 'D#': { active: false } },
   B: { B: { active: false }, C: { active: false }, 'C#': { active: false }, D: { active: false }, 'D#': { active: false }, E: { active: false }, F: { active: false }, 'F#': { active: false }, G: { active: false }, 'G#': { active: false }, A: { active: false }, 'A#': { active: false } },
@@ -25,6 +39,9 @@ const initialNotes = {
   E: { E: { active: false }, F: { active: false }, 'F#': { active: false }, G: { active: false }, 'G#': { active: false }, A: { active: false }, 'A#': { active: false }, B: { active: false }, C: { active: false }, 'C#': { active: false }, D: { active: false }, 'D#': { active: false } },
 };
 
+/**
+ * A mapping of selected chord tones for each string.
+ */
 const chordie: { [key: string]: string | null } = {
   E: null,
   A: null,
@@ -45,46 +62,90 @@ interface ChordInfo {
   intervalsObj: { [key: string]: string };
 }
 
+interface Notes {
+  [key: string]: {
+    active: boolean,
+    chordTone?: boolean,
+    relativeNote?: string
+  }
+}
 
 function App() {
   const [chords, setChords] = useState<{ [key: string]: ChordInfo }>({})
-  const [notes, setNotes] = useState<{ [key: string]: { [key: string]: { active: boolean, relativeNote?: string } } }>(initialNotes);
+  const [notes, setNotes] = useState<{ [key: string]: Notes }>(initialNotes);
   const [moreInfo, setMoreInfo] = useState(false);
   const [showNotes, setShowNotes] = useState(true);
-  // const [chordTones, setChordTones] = useState(false);
+  const [chordTones, setChordTones] = useState(false);
 
   const handleRadioChange = () => {
-    setShowNotes(!showNotes);
+    setShowNotes((prevShowNotes) => !prevShowNotes);
   };
 
-  // const handleShowNotes = () => {
+  /**
+   * Handles the change of checkbox to show chord tones.
+   * @param {boolean} update - Whether to update chord tones.
+   */
+  const handleShowNotes = (update?: boolean) => {
+    if (update) {
+      setChordTones((prevChordTones) => !prevChordTones); 
+      showChordTones(!chordTones);
+    } else {
+      showChordTones(chordTones);
+    }
+  };
 
-  //   setChordTones(!chordTones);
-  // }
+  /**
+   * Handles updating the notes state based on chord tones.
+   * @param {boolean} show - Whether to show chord tones.
+   */
+  const showChordTones = (show?: boolean) => {
+    for (const string of Object.values(notes)) {
+      for (const note of Object.keys(string)) {
+        for (const val of Object.values(chordie)) {
+          if (val && show) {
+            string[val].chordTone = true;
+          } else {
+            delete string[note].chordTone;
+          }
+        }
+      }
+    }
+  }
 
+  /**
+   * Handles the update of chord selection on the guitar fretboard.
+   * @param {string} string - The string on the guitar.
+   * @param {string} target - The target note on the string.
+   */
   const handleChordUpate = (string: string, target: string) => {
     const updatedNotes = { ...notes };
-    const currentValue = updatedNotes[string][target].active
+    const currentTargetState = updatedNotes[string][target].active
 
+    // Reset all notes on the selected string to be inactive
     for (const note in updatedNotes[string]) {
       updatedNotes[string][note].active = false;
     }
 
-    updatedNotes[string][target].active = !currentValue;
-    chordie[string] = !currentValue ? target : null;
+    // if (updatedNotes[string][target].chordTone) {} //? ----- whats the expected behaviour ???
 
+    // Toggle the activity status of the target note
+    updatedNotes[string][target].active = !currentTargetState;
+
+    // Update chordie with the selected or deselected chord tone
+    chordie[string] = !currentTargetState ? target : null;
+
+    // Filter out null values from chordie and detect chords based on selected chord tones
     const nonNullStringChordie: string[] = Object.values(chordie).filter((v): v is string => v !== null);
-
-    
-    const detectedChords = detectChord(nonNullStringChordie).length 
+    const detectedChords = detectChord(nonNullStringChordie).length
       ? detectChord(nonNullStringChordie)
-      : detectChord(nonNullStringChordie, { assumePerfectFifth: true })
+      : detectChord(nonNullStringChordie, { assumePerfectFifth: true }) //!----- my experimental
 
-    console.log(detectChord(nonNullStringChordie), detectChord(nonNullStringChordie, { assumePerfectFifth: true }))
+    // Initialize an object to store detected chord information
     const chordsObj: { [key: string]: ChordInfo } = {}
 
+    // Iterate through detected chords and extract chord information
     for (const idx in detectedChords) {
-      const overChord = detectedChords[idx].indexOf('/');
+      const overChord = detectedChords[idx].indexOf('/'); //!----- needs rewrite for better chord info accuracy
       const { name, aliases, intervals, notes, quality, type } = overChord <= 0
         ? getChordData(detectedChords[idx])
         : getChordData(detectedChords[idx].slice(0, overChord));
@@ -94,30 +155,34 @@ function App() {
         return obj;
       }, {} as { [key: string]: string });
 
+      // Store the extracted chord information in chordsObj
       chordsObj[idx] = {
         chord: detectedChords[idx],
         name, aliases, intervals, notes, quality, type, intervalsObj
       };
     }
 
+    // Convert a note to its equivalent with a different accidental (e.g., C## to D)
     const convertDouble = (note: string, type: string) => {
       const target = note.replace(type, '');
       return chromaticSharp[(chromaticSharp.indexOf(target) + 2) % chromaticSharp.length];
     }
 
     for (const val of Object.values(updatedNotes)) {
+      // Remove relativeNote properties from updatedNotes
       for (const _v of Object.values(val)) {
         delete _v.relativeNote;
       }
 
       if (chordsObj[0]?.notes) {
         for (const relNote of chordsObj[0].notes) {
-
+          
+          // Add relativeNote properties based on detected chord's notes
           if (relNote.includes('##')) {
             val[convertDouble(relNote, '##')].relativeNote = relNote;
           }
           if (relNote.includes('bb')) {
-            val[convertDouble(relNote, 'bb')].relativeNote = relNote;
+            val[convertDouble(relNote, 'bb')].relativeNote = relNote; //! ---- needs more testing
           }
 
           if (enharmonicMap[relNote]) {
@@ -126,6 +191,9 @@ function App() {
         }
       }
     }
+
+    // Update the display of chord tones, notes, and chord information
+    showChordTones(chordTones);
     setNotes(updatedNotes);
     setChords(chordsObj);
   };
@@ -156,7 +224,7 @@ function App() {
 
                 return (
                   <div
-                    className={`note ${_v.active ? 'active' : ''} ${isMobile ? 'mobile' : ''}`}
+                    className={`note ${_v.active || _v.chordTone ? 'active' : ''} ${isMobile ? 'mobile' : ''}`}
                     key={_i}
                     onClick={() =>
                       handleChordUpate(string, note)}
@@ -224,16 +292,16 @@ function App() {
             />
             Show Intervals
           </label>
-          {/* <label>
+          <label>
             <input
               type="checkbox"
               className="checkbox"
               id="myCheckbox"
               checked={chordTones}
-              onChange={handleShowNotes}
+              onChange={() => handleShowNotes(true)}
             />
             Show chord tones
-          </label> */}
+          </label>
         </div>
       </div>
     </div>
